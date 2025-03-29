cf46945165819574f54fdca9fc137f947a5c2d90
unknown <rizwankazi661@gmail.com>
2025-03-29 15:23:21 +0530

    fix: Layout issue fixed

--- src/screens/Transaction/Transaction.tsx
+++ src/screens/Transaction/Transaction.tsx
@@ -12,27 +12,29 @@ import TransactionTable from '../../components/TransactionTable/TransactionTable
 
 function Transaction() {
     return (
-        <div className={styles.main_container}>
-            <div className={styles.content}>
-                <header>
-                    <div>
-                        {constants.Transaction.title}
+        <div className={styles.parent_container}>
+            <div className={styles.main_container}>
+                <div className={styles.content}>
+                    <header>
+                        <div>
+                            {constants.Transaction.title}
+                        </div>
+                        <div className={styles.action_container}>
+                            <Button title={constants.Common.Button.matchReceipts} type={ButtonType.SECONDARY} leftImageSrc={ReceiptIcon} />
+                            <Button title={constants.Common.Button.exportAll} type={ButtonType.SECONDARY} leftImageSrc={DownloadIcon} />
+                        </div>
+                    </header>
+                    <div className={styles.filters_section}>
+                        <Button title={"Data Views"} type={ButtonType.PRIMARY} leftImageSrc={BookmarkIcon} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
+                        <hr />
+                        <Button title={constants.Common.Button.filters} type={ButtonType.SECONDARY} leftImageSrc={FilterIcon} />
+                        <Button title={"Date"} type={ButtonType.PRIMARY} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
+                        <Button title={"Keywords"} type={ButtonType.PRIMARY} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
+                        <Button title={"Amount"} type={ButtonType.PRIMARY} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
                     </div>
-                    <div className={styles.action_container}>
-                        <Button title={constants.Common.Button.matchReceipts} type={ButtonType.SECONDARY} leftImageSrc={ReceiptIcon} />
-                        <Button title={constants.Common.Button.exportAll} type={ButtonType.SECONDARY} leftImageSrc={DownloadIcon} />
+                    <div className={styles.table_container}>
+                        <TransactionTable />
                     </div>
-                </header>
-                <div className={styles.filters_section}>
-                    <Button title={"Data Views"} type={ButtonType.PRIMARY} leftImageSrc={BookmarkIcon} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
-                    <hr />
-                    <Button title={constants.Common.Button.filters} type={ButtonType.SECONDARY} leftImageSrc={FilterIcon} />
-                    <Button title={"Date"} type={ButtonType.PRIMARY} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
-                    <Button title={"Keywords"} type={ButtonType.PRIMARY} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
-                    <Button title={"Amount"} type={ButtonType.PRIMARY} rightImageSrc={KeyboardArrowDown} shape={ButtonShape.SQUARE} />
-                </div>
-                <div className={styles.table_container}>
-                    <TransactionTable />
                 </div>
             </div>
         </div>
